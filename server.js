c3e7378f4f74088535c6555b4202759261d00c50
Bashit Mustapha <mustaphabashit646@gmail.com>
2025-12-16 10:59:14 +0100

    cors issue fixed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = 5000
 const app = express();
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
