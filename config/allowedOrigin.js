8090fcc4f4c2aaf5ff903174c9579c272e85bdbe
Bashit Mustapha <mustaphabashit646@gmail.com>
2025-12-16 11:07:26 +0100

    live api configured

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,5 +1,5 @@
 const allowedOrigin = [
-    'https://code-book-zd73.vercel.app/',
+    'https://code-book-zd73.vercel.app',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
